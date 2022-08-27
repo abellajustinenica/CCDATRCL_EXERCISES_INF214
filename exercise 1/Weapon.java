b27f04455730f98cddef5e9220afaacaf4821513
Abella, Justine Nica <111735968+abellajustinenica@users.noreply.github.com>
2022-08-27 16:51:00 +0800

    Create Weapon.java

---
+++ exercise 1/Weapon.java
@@ -0,0 +1,13 @@
+public class Weapon {
+    //Weapon attributes
+    int damage;
+    int weight;
+    String rarity;
+    String name;
+    String description;
+
+    //Weapon method
+    public void weaponDescription() {
+        System.out.println("My object is " + name + " and I have " + description );
+    }
+}
